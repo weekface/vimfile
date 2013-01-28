7e204a3c3dd08479195fab20233895c443252444
WangJinzhong <andywang7259@gmail.com>
2013-01-28 21:42:56 +0800

    install font

--- install.sh
+++ install.sh
@@ -21,9 +21,9 @@ mkdir -p ~/.vim/vundle
 cd ~/.vim/vundle && git clone git://github.com/gmarik/vundle.git .
 vim +BundleInstall +qall
 
-# # install monaco font
-# cd ~/tmp
-# wget http://www.gringod.com/wp-upload/MONACO.TTF
-# sudo cp MONACO.TTF /usr/share/fonts/truetype/
-# git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ~/.fonts/ubuntu-mono-powerline-ttf
-# sudo fc-cache -f -v
+# install monaco font
+cd ~/tmp
+wget http://www.gringod.com/wp-upload/MONACO.TTF
+sudo cp MONACO.TTF /usr/share/fonts/truetype/
+git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ~/.fonts/ubuntu-mono-powerline-ttf
+sudo fc-cache -f -v
