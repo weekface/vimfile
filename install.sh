2f6a58415d92e564d695b0306b56787e9069df30
Chang <weekface@gmail.com>
2014-07-25 10:19:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,6 +34,12 @@ sudo cp MONACO.TTF /usr/share/fonts/truetype/
 git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ~/.fonts/ubuntu-mono-powerline-ttf
 sudo fc-cache -f -v
 
+cd ~/tmp
+wget http://www.gringod.com/wp-upload/MONACO.TTF
+sudo cp MONACO.TTF /usr/share/fonts/truetype/
+git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ~/.fonts/ubuntu-mono-powerline-ttf
+sudo fc-cache -f -v
+
 cp -r $GOROOT/misc/vim/* ~/.vim/
 go get -u github.com/nsf/gocode
 cd $GOPATH/src/github.com/nsf/gocode/vim
