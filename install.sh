d5c7a5ae669e2f18bd8ae3a40f120f9ef614761f
Chang <weekface@gmail.com>
2014-07-31 11:24:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ mkdir -p ~/.vim/vundle
 cd ~/.vim/vundle && git clone git://github.com/gmarik/vundle.git .
 vim +BundleInstall +qall
 
-rm -rf ~/.vim/bundle/snipmate.vim/snippets
+# rm -rf ~/.vim/bundle/snipmate.vim/snippets
 
 # install monaco font
 cd ~/tmp
@@ -34,30 +34,24 @@ sudo cp MONACO.TTF /usr/share/fonts/truetype/
 git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ~/.fonts/ubuntu-mono-powerline-ttf
 sudo fc-cache -f -v
 
-cd ~/tmp
-wget http://www.gringod.com/wp-upload/MONACO.TTF
-sudo cp MONACO.TTF /usr/share/fonts/truetype/
-git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ~/.fonts/ubuntu-mono-powerline-ttf
-sudo fc-cache -f -v
+# echo "下面为golang开发进行的特殊配置，包括："
+# echo "安装gocode自动提示"
+# echo "安装godef自动跳转"
+# echo "不需要的请勿安装!确定安装吗？(Y/N)"
 
-echo "下面为golang开发进行的特殊配置，包括："
-echo "安装gocode自动提示"
-echo "安装godef自动跳转"
-echo "不需要的请勿安装!确定安装吗？(Y/N)"
-
-read ANSWER
-case $ANSWER in
-[Yy])
-  source ~/.bashrc
-  cp -r $GOROOT/misc/vim/* ~/.vim/
-  go get -u github.com/nsf/gocode
-  go get code.google.com/p/rog-go/exp/cmd/godef
-  cd $GOPATH/src/github.com/nsf/gocode/vim
-  ./update.sh
-  gocode set propose-builtins true
-  echo "安装成功！"
-  ;;
-[Nn])
-  echo "已经跳过安装..."
-  ;;
-esac
+# read ANSWER
+# case $ANSWER in
+# [Yy])
+#  source ~/.bashrc
+#  cp -r $GOROOT/misc/vim/* ~/.vim/
+#  go get -u github.com/nsf/gocode
+#  go get code.google.com/p/rog-go/exp/cmd/godef
+#  cd $GOPATH/src/github.com/nsf/gocode/vim
+#  ./update.sh
+#  gocode set propose-builtins true
+#  echo "安装成功！"
+#  ;;
+# [Nn])
+#  echo "已经跳过安装..."
+#  ;;
+# esac
